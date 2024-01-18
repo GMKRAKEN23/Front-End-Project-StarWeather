7fab72313f6eb8be5556848af06ceff5f9a6c303
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-01-18 09:57:29 +0100

    qr code finished and add link a in logo

--- js/qrCode.js
+++ js/qrCode.js
@@ -1,9 +1,29 @@
+let containerCode = document.querySelector('.article_code_qr');
+let crossContainerCode = document.querySelector('.bx-x');
+
 // Generate QR Code
 let qrcode = new QRCode(document.getElementById("qrcode"), {
-	text: "https://webisora.com",
+	text: "index.html",
 	width: 120,
 	height: 120,
-	colorLight : "#ffffff",
-	correctLevel : QRCode.CorrectLevel.H
+	colorLight: "#ffffff",
+	correctLevel: QRCode.CorrectLevel.H
 });
 
+function displayCode() {
+	window.addEventListener('load', function () {
+
+		if (containerCode.style.right == "-100%") {
+			setInterval(() => {
+				containerCode.style.right = "2%";
+			}, 1000);
+		} else if (containerCode.style.right = "2%") {
+			    crossContainerCode.addEventListener('click', function () {
+				containerCode.style.right = "-100%";
+			})
+		}
+
+	})
+}
+
+displayCode();
